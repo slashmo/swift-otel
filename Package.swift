46b71807191cf4fb0e2d05416dbdf17df17427fe
Moritz Lang <hi@slashmo.codes>
2022-07-01 17:52:21 +0200

    Release 0.3.0 🔖

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "OtlpGRPCSpanExporting", targets: ["OtlpGRPCSpanExporting"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-distributed-tracing.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/apple/swift-distributed-tracing.git", .upToNextMinor(from: "0.3.0")),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
--- Sources/OpenTelemetry/OTel.swift
+++ Sources/OpenTelemetry/OTel.swift
@@ -18,7 +18,7 @@ import Tracing
 /// The main entry point to using OpenTelemetry.
 public final class OTel {
     /// The current `semver` version of the library.
-    public static let versionString = "0.1.0"
+    public static let versionString = "0.3.0"
 
     private let eventLoopGroup: EventLoopGroup
     private let resourceDetection: ResourceDetection

883db39171ae80dd7bbaa390d6e19eafc09c538b
Joannis Orlandos <joannis@orlandos.nl>
2022-11-09 16:58:38 +0100

    pdate Swift tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -22,9 +22,9 @@ let package = Package(
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "Run", dependencies: [
-            .target(name: "OpenTelemetry"),
-            .target(name: "OtlpGRPCExporter"),
+        .executableTarget(name: "Run", dependencies: [
+            "OpenTelemetry",
+            "OtlpGRPCExporter",
         ]),
         .target(name: "OpenTelemetry", dependencies: [
             .product(name: "Logging", package: "swift-log"),

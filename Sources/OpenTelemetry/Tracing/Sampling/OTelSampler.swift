2f29af161905fa178a45fa00705d70dfee298ad2
Moritz Lang <hi@slashmo.codes>
2023-06-13 20:16:22 -0700

    Add OTelSampler

---
+++ Sources/OpenTelemetry/Tracing/Sampling/OTelSampler.swift
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Tracing
+
+/// Decides whether a given span should be sampled.
+public protocol OTelSampler {
+    /// Request a sampling result for the given span values.
+    ///
+    /// - Note: The received values are all captured at the time of span creation.
+    /// Most of these values _may_ change after a sampling decision was made.
+    ///
+    /// - Parameters:
+    ///   - operationName: The span's operation name at time of creation.
+    ///   - kind: The span's kind.
+    ///   - traceID: The span's trace ID.
+    ///   - attributes: The span's attributes at time of creation.
+    ///   - links: The span's links at time of creation.
+    ///   - parentContext: The span's parent service context.
+    ///
+    /// - Returns: A result indicating whether to sample the span.
+    func samplingResult(
+        operationName: String,
+        kind: SpanKind,
+        traceID: OTelTraceID,
+        attributes: SpanAttributes,
+        links: [SpanLink],
+        parentContext: ServiceContext
+    ) -> OTelSamplingResult
+}

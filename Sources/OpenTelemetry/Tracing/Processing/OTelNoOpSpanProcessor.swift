85eb25140575e8bc0c6b6ac98b994af9f4f32178
Moritz Lang <hi@slashmo.codes>
2023-06-18 20:20:52 +0200

    Add OTelSpanProcessor

---
+++ Sources/OpenTelemetry/Tracing/Processing/OTelNoOpSpanProcessor.swift
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import ServiceContextModule
+
+/// A span processor that ignores all operations, used when no spans should be processed.
+public struct OTelNoOpSpanProcessor: OTelSpanProcessor {
+    /// Initialize a no-op span processor.
+    public init() {}
+
+    public func onStart(_ span: OTelSpan, parentContext: ServiceContext) {
+        // no-op
+    }
+
+    public func onEnd(_ span: OTelFinishedSpan) {
+        // no-op
+    }
+
+    public func forceFlush() async throws {
+        // no-op
+    }
+
+    public func shutdown() async throws {
+        // no-op
+    }
+}

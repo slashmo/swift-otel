28bf49672cdac8d87fcc46d725cbaeb94e346a25
Moritz Lang <hi@slashmo.codes>
2023-06-27 21:29:07 +0200

    Add license headers script

--- Sources/OpenTelemetry/Helpers/Hex.swift
+++ Sources/OpenTelemetry/Helpers/Hex.swift
@@ -1,8 +1,8 @@
 //===----------------------------------------------------------------------===//
 //
-// This source file is part of the swift-otel open source project
+// This source file is part of the Swift OTel open source project
 //
-// Copyright (c) 2023 Moritz Lang and the swift-otel project authors
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information

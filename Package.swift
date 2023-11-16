19de587a727f58139c6e2073a0783980d1e8ef60
Moritz Lang <hi@slashmo.codes>
2023-11-16 17:06:09 +0100

    Enable strict concurrency checking

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.9
 import PackageDescription
 
+let sharedSwiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
+
 let package = Package(
     name: "swift-otel",
     platforms: [
@@ -33,14 +35,16 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Tracing", package: "swift-distributed-tracing"),
                 .product(name: "DequeModule", package: "swift-collections"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
         .testTarget(
             name: "OpenTelemetryTests",
             dependencies: [
                 .target(name: "OpenTelemetry"),
                 .target(name: "OTelTesting"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
 
         .target(
@@ -48,7 +52,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Tracing", package: "swift-distributed-tracing"),
                 .target(name: "OpenTelemetry"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
 
         // MARK: - OTLP
@@ -58,14 +63,16 @@ let package = Package(
             dependencies: [
                 .target(name: "OpenTelemetry"),
                 .target(name: "OTLPGRPC"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
         .testTarget(
             name: "OTLPTests",
             dependencies: [
                 .target(name: "OTLP"),
                 .target(name: "OTelTesting"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
 
         .target(
@@ -73,14 +80,16 @@ let package = Package(
             dependencies: [
                 .target(name: "OpenTelemetry"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
         .testTarget(
             name: "OTLPCoreTests",
             dependencies: [
                 .target(name: "OTLPCore"),
                 .target(name: "OTelTesting"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
 
         .target(
@@ -93,14 +102,16 @@ let package = Package(
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "NIOHTTP2", package: "swift-nio-http2"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
         .testTarget(
             name: "OTLPGRPCTests",
             dependencies: [
                 .target(name: "OTLPGRPC"),
                 .target(name: "OTelTesting"),
-            ]
+            ],
+            swiftSettings: sharedSwiftSettings
         ),
     ]
 )

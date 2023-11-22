4ba0c3d5d833ed95ee8b3b8da969faaa4d32c748
Moritz Lang <hi@slashmo.codes>
2023-11-22 14:14:35 +0100

    Add MultiplexPropagator

---
+++ Sources/OpenTelemetry/Tracing/Propagating/OTelMultiplexPropagator.swift
@@ -0,0 +1,56 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Instrumentation
+
+/// A pseudo-`OTelPropagator` that may be used to instrument using
+/// multiple other `OTelPropagator`s across a common `OTel.SpanContext`.
+public struct OTelMultiplexPropagator: OTelPropagator {
+    private let propagators: [OTelPropagator]
+
+    /// Create a `MultiplexPropagator`.
+    ///
+    /// ## Extraction Priority
+    ///
+    /// The order of the given propagators matters when extracting!
+    /// If multiple propagators successfully extract, the extracted span context of
+    /// the last propagator called, i.e. the last propagator in the `propagators` array.
+    ///
+    /// - Parameter propagators: An array of `OTelPropagator`s, each of which
+    /// will be used to `inject`/`extract` through the same `SpanContext`.
+    public init(_ propagators: [OTelPropagator]) {
+        self.propagators = propagators
+    }
+
+    public func extractSpanContext<Carrier, Extract>(
+        from carrier: Carrier,
+        using extractor: Extract
+    ) throws -> OTelSpanContext? where Extract: Extractor, Carrier == Extract.Carrier {
+        var spanContext: OTelSpanContext?
+        for propagator in propagators {
+            guard let context = try propagator.extractSpanContext(from: carrier, using: extractor) else {
+                continue
+            }
+            spanContext = context
+        }
+        return spanContext
+    }
+
+    public func inject<Carrier, Inject>(
+        _ spanContext: OTelSpanContext,
+        into carrier: inout Carrier,
+        using injector: Inject
+    ) where Inject: Injector, Carrier == Inject.Carrier {
+        propagators.forEach { $0.inject(spanContext, into: &carrier, using: injector) }
+    }
+}
---
+++ Tests/OpenTelemetryTests/Tracing/Propagating/OTelMultiplexPropagatorTests.swift
@@ -0,0 +1,170 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Instrumentation
+@testable import OpenTelemetry
+import XCTest
+
+final class OTelMultiplexPropagatorTests: XCTestCase {
+    private let injector = DictionaryInjector()
+    private let extractor = DictionaryExtractor()
+
+    // MARK: - Inject
+
+    func test_invokesInjectOnAllPropagators() {
+        let spanContext = OTelSpanContext(
+            traceID: .oneToSixteen,
+            spanID: .oneToEight,
+            parentSpanID: nil,
+            traceFlags: [],
+            traceState: nil,
+            isRemote: false
+        )
+        var headers = [String: String]()
+
+        let propagator = OTelMultiplexPropagator([SystemAPropagator(), SystemBPropagator()])
+        propagator.inject(spanContext, into: &headers, using: injector)
+
+        XCTAssertEqual(headers["a-trace-id"], "0102030405060708090a0b0c0d0e0f10-0102030405060708")
+        XCTAssertEqual(headers["b-trace-id"], "100f0e0d0c0b0a090807060504030201-0807060504030201")
+    }
+
+    // MARK: - Extract
+
+    func test_extractsNil_allPropagatorsReturningNil() throws {
+        let headers = ["c-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemAPropagator(), SystemBPropagator()])
+        XCTAssertNil(try propagator.extractSpanContext(from: headers, using: extractor))
+    }
+
+    func test_extractsSystemAHeader() throws {
+        let headers = ["a-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemAPropagator(), SystemBPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemAPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemAPropagator.validSpanID)
+    }
+
+    func test_extractsSystemAHeader_reverseOrder() throws {
+        let headers = ["a-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemBPropagator(), SystemAPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemAPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemAPropagator.validSpanID)
+    }
+
+    func test_extractsSystemBHeader() throws {
+        let headers = ["b-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemAPropagator(), SystemBPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemBPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemBPropagator.validSpanID)
+    }
+
+    func test_extractsSystemBHeader_reverseOrder() throws {
+        let headers = ["b-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemBPropagator(), SystemAPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemBPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemBPropagator.validSpanID)
+    }
+
+    func test_extractsBothSystemHeaders() throws {
+        let headers = ["a-trace-id": "valid", "b-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemAPropagator(), SystemBPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemBPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemBPropagator.validSpanID)
+    }
+
+    func test_extractsBothSystemHeaders_reverseOrder() throws {
+        let headers = ["a-trace-id": "valid", "b-trace-id": "valid"]
+
+        let propagator = OTelMultiplexPropagator([SystemBPropagator(), SystemAPropagator()])
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID, SystemAPropagator.validTraceID)
+        XCTAssertEqual(spanContext.spanID, SystemAPropagator.validSpanID)
+    }
+}
+
+private struct SystemAPropagator: OTelPropagator {
+    static let validTraceID = OTelTraceID.oneToSixteen
+    static let validSpanID = OTelSpanID.oneToEight
+
+    public func extractSpanContext<Carrier, Extract>(
+        from carrier: Carrier,
+        using extractor: Extract
+    ) throws -> OTelSpanContext? where Extract: Extractor, Carrier == Extract.Carrier {
+        guard let value = extractor.extract(key: "a-trace-id", from: carrier) else { return nil }
+        guard value == "valid" else { throw PropagatorError() }
+        return OTelSpanContext(
+            traceID: Self.validTraceID,
+            spanID: Self.validSpanID,
+            parentSpanID: nil,
+            traceFlags: [],
+            traceState: nil,
+            isRemote: true
+        )
+    }
+
+    public func inject<Carrier, Inject>(
+        _ spanContext: OTelSpanContext,
+        into carrier: inout Carrier,
+        using injector: Inject
+    ) where Inject: Injector, Carrier == Inject.Carrier {
+        injector.inject("\(Self.validTraceID)-\(Self.validSpanID)", forKey: "a-trace-id", into: &carrier)
+    }
+}
+
+private struct SystemBPropagator: OTelPropagator {
+    static let validTraceID = OTelTraceID(bytes: (16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
+    static let validSpanID = OTelSpanID(bytes: (8, 7, 6, 5, 4, 3, 2, 1))
+
+    public func extractSpanContext<Carrier, Extract>(
+        from carrier: Carrier,
+        using extractor: Extract
+    ) throws -> OTelSpanContext? where Extract: Extractor, Carrier == Extract.Carrier {
+        guard let value = extractor.extract(key: "b-trace-id", from: carrier) else { return nil }
+        guard value == "valid" else { throw PropagatorError() }
+        return OTelSpanContext(
+            traceID: Self.validTraceID,
+            spanID: Self.validSpanID,
+            parentSpanID: nil,
+            traceFlags: [],
+            traceState: nil,
+            isRemote: true
+        )
+    }
+
+    public func inject<Carrier, Inject>(
+        _ spanContext: OTelSpanContext,
+        into carrier: inout Carrier,
+        using injector: Inject
+    ) where Inject: Injector, Carrier == Inject.Carrier {
+        injector.inject("\(Self.validTraceID)-\(Self.validSpanID)", forKey: "b-trace-id", into: &carrier)
+    }
+}
+
+private struct PropagatorError: Error {}

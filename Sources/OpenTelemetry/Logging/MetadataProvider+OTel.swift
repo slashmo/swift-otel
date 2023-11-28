e5d9e752950d5a58f7183cca6a4a259fcae1d823
Moritz Lang <hi@slashmo.codes>
2023-11-28 23:00:39 +0100

    Add trace flags & parent span ID to metadata provider

--- Sources/OpenTelemetry/Logging/MetadataProvider+OTel.swift
+++ Sources/OpenTelemetry/Logging/MetadataProvider+OTel.swift
@@ -17,17 +17,31 @@ import ServiceContextModule
 extension Logger.MetadataProvider {
     /// A metadata provider exposing the current trace and span ID.
     ///
+    /// [OpenTelemetry Specification: Trace Context Fields](https://github.com/open-telemetry/opentelemetry-specification/blob/v1.20.0/specification/logs/data-model.md#trace-context-fields)
+    ///
     /// - Parameters:
-    ///   - traceIDKey: The metadata key of the trace ID. Defaults to "trace-id".
-    ///   - spanIDKey: The metadata key of the span ID. Defaults to "span-id".
+    ///   - traceIDKey: The metadata key of the trace ID. Defaults to `"trace_id"`.
+    ///   - spanIDKey: The metadata key of the span ID. Defaults to `"span_id"`.
+    ///   - traceFlagsKey: The metadata key of the trace flags. Defaults to `"trace_flags"`.
+    ///   - parentSpanIdKey: The metadata key of the parent span ID. Defaults to `nil`, i.e. not included.
     /// - Returns: A metadata provider ready to use with Logging.
-    public static func otel(traceIDKey: String = "trace-id", spanIDKey: String = "span-id") -> Logger.MetadataProvider {
+    public static func otel(
+        traceIDKey: String = "trace_id",
+        spanIDKey: String = "span_id",
+        traceFlagsKey: String = "trace_flags",
+        parentSpanIDKey: String? = nil
+    ) -> Logger.MetadataProvider {
         .init {
             guard let spanContext = ServiceContext.current?.spanContext else { return [:] }
-            return [
+            var metadata: Logger.Metadata = [
                 traceIDKey: "\(spanContext.traceID)",
                 spanIDKey: "\(spanContext.spanID)",
+                traceFlagsKey: "\(spanContext.traceFlags.rawValue)",
             ]
+            if let parentSpanIDKey, let parentSpanID = spanContext.parentSpanID {
+                metadata[parentSpanIDKey] = "\(parentSpanID)"
+            }
+            return metadata
         }
     }
 

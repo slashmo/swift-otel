b2d131d87b7f4e9fb0c08a65a96fd49edad72e25
Joannis Orlandos <joannis@orlandos.nl>
2024-03-25 14:14:05 +0100

    Add OTelLogHandler that sends OTelLogEntry to an OTelLogEntryProcessor

---
+++ Sources/OTel/Logging/Processing/OTelNoOpLogEntryProcessor.swift
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2024 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import ServiceContextModule
+
+/// A log entry processor that ignores all operations, used when no logs should be processed.
+@_spi(Logging)
+public struct OTelNoOpLogEntryProcessor: OTelLogEntryProcessor, CustomStringConvertible {
+    public let description = "OTelNoOpLogEntryProcessor"
+
+    private let stream: AsyncStream<Void>
+    private let continuation: AsyncStream<Void>.Continuation
+
+    /// Initialize a no-op log entry processor.
+    public init() {
+        (stream, continuation) = AsyncStream.makeStream()
+    }
+
+    public func run() async {
+        for await _ in stream.cancelOnGracefulShutdown() {}
+    }
+
+    public func onLog(_ log: OTelLogEntry) {
+        // no-op
+    }
+
+    public func forceFlush() async throws {
+        // no-op
+    }
+}

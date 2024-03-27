4a2c64f98333520cf3fb4e49054bd4646c1e4531
Moritz Lang <hi@slashmo.codes>
2024-03-27 23:16:12 +0100

    Add OTelInMemoryLogRecordProcessor for testing

---
+++ Sources/OTelTesting/OTelInMemoryLogRecordProcessor.swift
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2024 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_spi(Logging) import OTel
+import NIOConcurrencyHelpers
+
+/// An in-memory log record processor, collecting emitted log records into ``onEmit(_:)``.
+package final class OTelInMemoryLogRecordProcessor: OTelLogRecordProcessor {
+    package var records: [OTelLogRecord] { _records.withLockedValue { $0 } }
+
+    private let _records = NIOLockedValueBox([OTelLogRecord]())
+    private let _numberOfShutdowns = NIOLockedValueBox(0)
+    private let _numberOfForceFlushes = NIOLockedValueBox(0)
+
+    private let stream: AsyncStream<Void>
+    private let continuation: AsyncStream<Void>.Continuation
+
+    package init() {
+        (stream, continuation) = AsyncStream.makeStream()
+    }
+
+    package func run() async throws {
+        for await _ in stream.cancelOnGracefulShutdown() {}
+        _numberOfShutdowns.withLockedValue { $0 += 1 }
+    }
+
+    package func onEmit(_ record: OTelLogRecord) {
+        _records.withLockedValue { $0.append(record) }
+    }
+
+    package func forceFlush() async throws {
+        _numberOfForceFlushes.withLockedValue { $0 += 1 }
+    }
+}

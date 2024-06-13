ef590f0430ae5de0c109443898b3b28ee2178ff6
Joannis Orlandos <joannis@orlandos.nl>
2024-06-13 14:18:54 +0200

    Add OTel Log processing

---
+++ Sources/OTel/Logging/Processing/OTelSimpleLogEntryProcessor.swift
@@ -0,0 +1,46 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_spi(Logging)
+public struct OTelSimpleLogEntryProcessor<Exporter: OTelLogRecordExporter>: OTelLogRecordProcessor {
+    private let exporter: Exporter
+    private let stream: AsyncStream<OTelLogRecord>
+    private let continuation: AsyncStream<OTelLogRecord>.Continuation
+
+    public init(exporter: Exporter) {
+        self.exporter = exporter
+        (stream, continuation) = AsyncStream.makeStream()
+    }
+
+    public func run() async throws {
+        for try await record in stream.cancelOnGracefulShutdown() {
+            do {
+                try await exporter.export([record])
+            } catch {
+                // simple log processor does not attempt retries
+            }
+        }
+    }
+
+    public func onEmit(_ record: inout OTelLogRecord) {
+        continuation.yield(record)
+    }
+
+    public func forceFlush() async throws {
+        try await exporter.forceFlush()
+    }
+
+    public func shutdown() async throws {
+        await exporter.shutdown()
+    }
+}
